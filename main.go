26ef7cec3d35a8b57777616bfcfa5f4c355528d2
makarovandrej <makarov.andrey.nn@icloud.com>
2023-10-22 14:51:14 +0200

    Init commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// During of initialization of package
+func init() {
+	fmt.Printf("Main package initialized at %s\n", time.Now())
+}
+
+func main() {
+
+}
